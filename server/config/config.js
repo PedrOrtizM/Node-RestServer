5a4a6627b0ff5d70bc05f9dd0927174d48a2ca86
Pedro <pedrortizm18_@hotmail.com>
2018-11-13 14:33:42 -0500

    Agregando server

---
+++ server/config/config.js
@@ -0,0 +1,7 @@
+//process es un objeto global que está en toda la application
+
+
+// =========================
+//          PUERTO
+// =========================
+process.env.PORT = process.env.PORT || 3000;
